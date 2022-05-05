8666eff24ac0f16eae9001ef1fc9e282c7299150
demitriaswilbert <demitriaswilbert@gmail.com>
2022-05-05 15:41:49 +0700

    added minus, constants

--- ShuntingYard.py
+++ ShuntingYard.py
@@ -105,8 +105,13 @@ def cnmt(a):
 
 queue = []
 opstack = []
+start_token = {
+    'type' : 'start'
+}
+tokens_processed = [start_token]
 
 def process(token):
+    tokens_processed.append(token)
     if token['type'] == 'number':
         queue.append(token)
     elif token['type'] == 'function':
@@ -127,14 +132,14 @@ def process(token):
             while(((len(opstack) > 0) and (token['precedence'] <= opstack[-1]['precedence'])) and (token['assoc'] == 'left')):
                 queue.append(opstack.pop())
             opstack.append(token)
-    print('in :', token['value'])
-    print('queue: ', end='')
-    for i in range(len(queue)):
-        print(queue[i]['value'], end=' ')
-    print('\nstack: ', end='')
-    for i in range(len(opstack)):
-        print(opstack[i]['value'], end=' ')
-    print()
+    # print('in :', token['value'])
+    # print('queue: ', end='')
+    # for i in range(len(queue)):
+    #     print(queue[i]['value'], end=' ')
+    # print('\nstack: ', end='')
+    # for i in range(len(opstack)):
+    #     print(opstack[i]['value'], end=' ')
+    # print()
 
 def post_process():
     while len(opstack) > 0:
@@ -156,22 +161,36 @@ funcs = [
     'sin',
     'cos',
     'tan',
-    'pi',
     'round'
 ]
+
 funcs_token = [
     sin_token,
     cos_token,
     tan_token,
-    pi_token,
     round_token
 ]
 
-a = str(input())
+consts = [
+    'pi',
+]
 
+consts_token = [
+    pi,
+]
+
+
+a = str(input())
+negative = 1
 while(len(a)):
     if a[0] in symbols:
-        process(symbols_token[symbols.index(a[0])])
+        if (a[0] == '-') and (tokens_processed[-1] == openbrace_token or tokens_processed[-1] == plus_token or tokens_processed[-1] == multiply_token or tokens_processed[-1] == divide_token or tokens_processed[-1] == pow_token or tokens_processed[-1] == start_token):
+            negative = -1
+            a = a[1:]
+            continue
+        
+        token = symbols_token[symbols.index(a[0])]
+        process(token)
         a = a[1:]
         continue
     elif a[0] in '1234567890':
@@ -181,14 +200,24 @@ while(len(a)):
                 break
             cnt += 1
         num = a[0:cnt]
-        process(cnmt(float(num)))
+        process(cnmt(float(num)*negative))
+        negative = 1
         a = a[cnt:]
         continue
     elif a[0] in "qwertyuiopasdfghjklzxcvbnm":
         for i in range(len(funcs)):
             if(a.find(funcs[i]) == 0):
+                if(negative == -1):
+                    process(cnmt(-1))
+                    process(multiply_token)
+                    negative = 1
                 process(funcs_token[i])
                 a = a[len(funcs[i]):]
+                continue
+        for i in range(len(consts)):
+            if(a.find(consts[i]) == 0):
+                process(cnmt(-consts_token[i] if negative == -1 else consts_token[i]))
+                a = a[len(consts[i]):]
                 continue
     else:
         a = a[1:]
@@ -208,13 +237,16 @@ for i in range(len(queue)):
                 if queue[k]['type'] == 'number':
                     operands.append(k)
                     queue[k]['type'] = 'used'
+                    print('used', queue[k]['value'])
                     break
         operandsval = []
         for j in range(len(operands)):
             operandsval.append(queue[operands[j]]['value'])
+        print(queue[i]['value'])
+        print(operandsval)
         queue[i] = cnmt(queue[i]['func'](operandsval))
         print('queue: ', end='')
         for i in range(len(queue)):
-            if queue[i]['type'] == 'used': continue
+            if queue[i]['type'] == 'used': print('U',end='')
             print(queue[i]['value'], end=' ')
         print()
